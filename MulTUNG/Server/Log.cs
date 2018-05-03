c21ca67fd4d8cb75e44963c95708fd32d455729a
Felipe Martínez <pipernene@gmail.com>
2018-05-03 19:24:09 +0200

    Improved server logging

--- MulTUNG/Server/Log.cs
+++ MulTUNG/Server/Log.cs
@@ -1,32 +1,14 @@
-﻿using System;
+﻿using PiTung.Console;
+using System;
 using System.Collections.Generic;
 
 namespace Server
 {
     public static class Log
     {
-        public static Queue<string> LineQueue = new Queue<string>();
-
-        public static bool WriteToConsole { get; set; } = true;
-
-        public static void FlushQueue()
-        {
-            while (LineQueue.Count > 0)
-            {
-                Console.WriteLine(LineQueue.Dequeue());
-            }
-        }
-
         public static void WriteLine(string line)
         {
-            if (WriteToConsole)
-            {
-                Console.WriteLine(line);
-            }
-            else
-            {
-                LineQueue.Enqueue(line);
-            }
+            IGConsole.Log("[SERVER] " + line);
         }
     }
 }
