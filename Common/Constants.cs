328c6b4da26f290a98b2154370c431b3f69427c3
Felipe Martínez <pipernene@gmail.com>
2018-05-03 18:45:53 +0200

    Move

--- Common/Constants.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Common
-{
-    public static class Constants
-    {
-        public const int Port = 5678;
-    }
-}
--- Common/NetState.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Common
-{
-    public class NetState
-    {
-        public const int BufferSize = 4096;
-        public byte[] Buffer { get; set; } = new byte[BufferSize];
-    }
-}
